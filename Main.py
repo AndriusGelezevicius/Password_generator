fe0057ca81ffa6fe678073536a5c9b280e59279f
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-07-13 09:36:30 +0300

    made a main label

--- Main.py
+++ Main.py
@@ -1,10 +1,21 @@
 import tkinter as tk
+from tkinter import font
 
 
 # Create the main window
 root = tk.Tk()
 root.title("Password generator")
 root.geometry("400x450")
+root.resizable(False, False)
+
+custum_font = font.Font(size = 12, weight="bold")
+label = tk.Label(root, text="Password Generator", font=custum_font)
+label.pack(pady = 20)
+
+
+
+
+
 
 
 
