ae73a1d94e69d2b67315655df23c9f7c574efade
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-07-13 09:51:48 +0300

    Add frame and put password and button in it

--- Main.py
+++ Main.py
@@ -8,12 +8,21 @@ root.title("Password generator")
 root.geometry("400x450")
 root.resizable(False, False)
 
-custum_font = font.Font(size = 12, weight="bold")
-label = tk.Label(root, text="Password Generator", font=custum_font)
-label.pack(pady = 20)
+# Main Label
+custom_font = font.Font(size=12, weight="bold")
+label = tk.Label(root, text="Password Generator", font=custom_font)
+label.pack(pady=20)
 
+# Frame of button and entry
+frame = tk.Frame(root)
+frame.pack(pady=10, padx=10)
+# Showing password entry
+password_show = tk.Entry(frame)
+password_show.pack(side=tk.LEFT, padx=(20,20))
 
-
+# Copy button
+button_copy = tk.Button(frame, text="Copy", command="copy_password")
+button_copy.pack(side=tk.LEFT)
 
 
 
