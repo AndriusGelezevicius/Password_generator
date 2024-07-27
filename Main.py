421a63540cc817e52854e863f3bf9ab33b88ee86
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-07-27 19:15:56 +0300

    made more modern with tkk

--- Main.py
+++ Main.py
@@ -1,7 +1,7 @@
 import random
 import string
 import tkinter as tk
-from tkinter import font
+from tkinter import font, ttk
 
 def copy_password():
     password_value = password_show.get()
@@ -46,31 +46,31 @@ label = tk.Label(root, text="Password Generator", font=custom_font)
 label.pack(pady=20)
 
 # Frame of button and entry
-frame = tk.Frame(root)
+frame = ttk.Frame(root)
 frame.pack(pady=10, padx=10)
 # Showing password entry
-password_show = tk.Entry(frame)
+password_show = ttk.Entry(frame)
 password_show.pack(side=tk.LEFT, padx=(20,20))
 
 # Copy button
-button_copy = tk.Button(frame, text="Copy", command=copy_password)
+button_copy = ttk.Button(frame, text="Copy", command=copy_password)
 button_copy.pack(side=tk.LEFT)
 
 
 # Slider
-slider_frame = tk.Frame(root)
+slider_frame = ttk.Frame(root)
 slider_frame.pack(pady=10, padx=10)
 
-slider_label = tk.Label(slider_frame, text="Password length: ")
+slider_label = ttk.Label(slider_frame, text="Password length: ")
 slider_label.pack(side=tk.LEFT, padx=(20,20))
 
 scale = tk.Scale(slider_frame, from_=5, to=40, orient="horizontal", command=checkbox_state)
 scale.pack(side=tk.LEFT)
 
 # Combobox
-combo_frame = tk.Frame(root)
+combo_frame = ttk.Frame(root)
 combo_frame.pack(pady=10, padx=10)
-combo_label = tk.Label(combo_frame, text="Characters used: ")
+combo_label = ttk.Label(combo_frame, text="Characters used: ")
 combo_label.pack(side=tk.LEFT, padx=(20,20))
 
 check_vars =[]
@@ -79,7 +79,7 @@ checkbox_labels = ["ABC", "abc", "123", "#$&"]
 for labels in checkbox_labels:
     check_var = tk.IntVar(value=1)  # Cinteger variable that Tkinter can use to track the state of the checkbox.
     check_vars.append(check_var)
-    check_box = tk.Checkbutton(combo_frame, text=labels, variable=check_var, command=checkbox_state)
+    check_box = ttk.Checkbutton(combo_frame, text=labels, variable=check_var, command=checkbox_state)
     check_box.pack(side=tk.LEFT)
 
 
