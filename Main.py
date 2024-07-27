9b424808a07436d2c73595ed67ec9685f9038d79
AndriusGelezevicius <andriusgelezevicius@gmail.com>
2024-07-27 16:36:52 +0300

    add checkbuttons

--- Main.py
+++ Main.py
@@ -18,7 +18,7 @@ def show_random_characters():
 root = tk.Tk()
 root.title("Password generator")
 root.geometry("400x450")
-root.resizable(False, False)
+#root.resizable(False, False)
 
 # Main Label
 custom_font = font.Font(size=12, weight="bold")
@@ -36,9 +36,33 @@ password_show.pack(side=tk.LEFT, padx=(20,20))
 button_copy = tk.Button(frame, text="Copy", command=show_random_characters)
 button_copy.pack(side=tk.LEFT)
 
+
 # Slider
-scale = tk.Scale(root, from_=5, to=40, orient="horizontal")
-scale.pack(pady=20)
+slider_frame = tk.Frame(root)
+slider_frame.pack(pady=10, padx=10)
+
+slider_label = tk.Label(slider_frame, text="Password length: ")
+slider_label.pack(side=tk.LEFT, padx=(20,20))
+
+scale = tk.Scale(slider_frame, from_=5, to=40, orient="horizontal")
+scale.pack(side=tk.LEFT)
+
+# Combobox
+combo_frame = tk.Frame(root)
+combo_frame.pack(pady=10, padx=10)
+combo_label = tk.Label(combo_frame, text="Characters used: ")
+combo_label.pack(side=tk.LEFT, padx=(20,20))
+
+check_vars =[]
+
+checkbox_labels = ["ABC", "abc", "123", "#$&"]
+for labels in checkbox_labels:
+    check_var = tk.IntVar()  # Cinteger variable that Tkinter can use to track the state of the checkbox.
+    check_vars.append(check_var)
+    check_box = tk.Checkbutton(combo_frame, text=labels, variable=check_var)
+    check_box.pack(side=tk.LEFT)
+
+
 
 show_random_characters()
 root.mainloop()
